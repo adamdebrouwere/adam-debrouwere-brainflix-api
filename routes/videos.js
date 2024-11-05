75aac3f6e493548fc3f211febf2ef13cdfba66b7
adamdebrouwere <adamdebrouwere@hotmail.com>
2024-11-05 16:14:58 -0600

    feat: router.get videos data

--- routes/videos.js
+++ routes/videos.js
@@ -1,7 +1,30 @@
 import express from 'express';
+import fs from 'fs';
 const router = express.Router();
 
 router.get("/videos", (req, res) => {
-    res.send("this is my router")
-})
+    fs.readFile("data/videos.json", "utf8", (err, data) => {
+        if (err) {
+            console.error("Error reading data from file:", err)
+            return res.status(500).json({
+                error: "Error reading file",
+            });
+        }
+
+        try {
+            const jsonData = JSON.parse(data);
+            res.json(jsonData);
+        } catch (err) {
+            console.error("Error parsing data:", err)
+            res.status(500).json({
+                error: 'Error parsing data'
+            })
+        }
+    })
+});
+
+router.get("/videos/:id", (req, res) => {
+    res.send("this is to get videos by thier id's")
+});
+
 export default router;
